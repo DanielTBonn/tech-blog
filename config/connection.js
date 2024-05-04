3d4a787e8c1d549ae03bad3c16768b93b57128d0
Daniel Bonn <danieltbonn@gmail.com>
2024-05-04 16:31:36 -0500

    added ability to change dialect of sequelize

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
     port: 3306,
   }
 );

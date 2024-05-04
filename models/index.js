dda7b3791486a1d30865daf31be9f00d651cec6a
Daniel Bonn <danieltbonn@gmail.com>
2024-05-04 18:11:35 -0500

    another deploy test

--- models/index.js
+++ models/index.js
@@ -3,28 +3,28 @@ const BlogPost = require('./BlogPost');
 const Comment = require('./Comment');
 
 // BlogPost has one User
-BlogPost.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+// BlogPost.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
-// User has many blogposts 
-User.hasMany(BlogPost, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+// // User has many blogposts 
+// User.hasMany(BlogPost, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
-// Comment belongs to user
-Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-})
+// // Comment belongs to user
+// Comment.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// })
 
-// Blogpost has many comments
-BlogPost.hasMany(Comment, {
-    foreignKey: 'blogpost_id',
-    onDelete: 'CASCADE'
-})
+// // Blogpost has many comments
+// BlogPost.hasMany(Comment, {
+//     foreignKey: 'blogpost_id',
+//     onDelete: 'CASCADE'
+// })
 
 module.exports = {
     User,

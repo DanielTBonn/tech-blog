0542c421bcd10663f8c902bc73098a986ca1842d
Daniel Bonn <danieltbonn@gmail.com>
2024-05-04 20:25:15 -0500

    reverted and added primary key to user_id

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,48 +1,48 @@
 // Blogpost model for sql
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
-// const User = require('./User')
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const User = require('./User')
 
-// class BlogPost extends Model {}
+class BlogPost extends Model {}
 
-// BlogPost.init(
-//     {
-//         id: {
-//             type: DataTypes.INTEGER,
-//             unique: true,
-//             allowNull: false,
-//             primaryKey: true,
-//             autoIncrement: true
-//         }, 
-//         title: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-//         content: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-//         username: {
-//             type: DataTypes.STRING,
-//             unique: true,
-//             allowNull: false
-//         },
-//         user_id: {
-//             type: DataTypes.INTEGER,
-//             unique: true,
-//             references: {
-//                 model: User,
-//                 key: 'id',
-//             }
-//         }
-//     },
-//     {
-//         sequelize,
-//         timestamps: true,
-//         freezeTableName: true,
-//         underscored: true,
-//         modelName: 'blogpost'
-//     }
-// );
+BlogPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            unique: true,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        }, 
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            references: {
+                model: User,
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogpost'
+    }
+);
 
-// module.exports = BlogPost;
+module.exports = BlogPost;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const {User, BlogPost} = require('./User');
-// const BlogPost = require('./BlogPost');
+const User = require('./User');
+const BlogPost = require('./BlogPost');
 const Comment = require('./Comment');
 
 // BlogPost has one User
